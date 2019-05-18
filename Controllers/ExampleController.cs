73f0dd6960c377af1f23fd5d873ba7a8fcb80802
unknown <linhlinhh2601@.com>
2019-05-18 22:05:16 +0700

    first commit

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookstore.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ActionResult Index(string name,int intnum =1)
+        {
+            ViewBag.Message = "Hello "+ name;
+            ViewBag.intnum = intnum;
+            return View();
+        }
+    }
+}
